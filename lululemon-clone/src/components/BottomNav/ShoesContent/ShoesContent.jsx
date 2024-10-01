896c7827943463cee903c3257923d36f261fad23
pandeyakash <pandeyakash0502@gmail.com>
2024-10-01 11:18:25 +0530

    Completed shoes dropdown

--- lululemon-clone/src/components/BottomNav/ShoesContent/ShoesContent.jsx
+++ lululemon-clone/src/components/BottomNav/ShoesContent/ShoesContent.jsx
@@ -1,16 +1,18 @@
+import "./ShoesContent.css";
+
 export const ShoesContent = () => {
   return (
     <div className="shoes_content">
       <div className="upper-shoes_content">
         <div className="left-upper-shoes_content">
-          <div>
+          <div className="left-left-upper-shoes_content">
             <ul>
               <li>SHOES BY GENDER</li>
               <li>Women's Shoes</li>
               <li>Men's Shoes</li>
             </ul>
           </div>
-          <div>
+          <div className="right-left-upper-shoes_content">
             <ul>
               <li>SHOE TYPES</li>
               <li>Cross Training Shoes</li>
@@ -29,15 +31,17 @@ export const ShoesContent = () => {
           <span>Shop Shoes</span>
         </div>
       </div>
-      <div className="lower-shoes_content">
-        <div className="left-lower-shoes_content">ACTIVITY</div>
-        <div className="middle-lower-shoes_content">
-          <span>Running</span>
-          <span>Workout</span>
-          <span>Casual</span>
-          <span>Hiking</span>
+      <div className="lower-shoes">
+        <div className="lower-shoes_content">
+          <div className="left-lower-shoes_content">ACTIVITY</div>
+          <div className="middle-lower-shoes_content">
+            <span>Running</span>
+            <span>Workout</span>
+            <span>Casual</span>
+            <span>Hiking</span>
+          </div>
+          <div className="right-lower-shoes_content">SHOP ALL SHOES</div>
         </div>
-        <div className="right-lower-shoes_content">SHOP ALL SHOES</div>
       </div>
     </div>
   );
